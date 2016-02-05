d74e193ad3a185a8b7349d79cd07aec8a3eceab5
Sobo <sobolevski.a@gmail.com>
2016-02-04 22:25:57 -0500

    added try excepts to handle failures

--- yahoo_ff.py
+++ yahoo_ff.py
@@ -27,81 +27,106 @@ class yahoo_ff:
 
     def __construct_incomestatement_annual(self):
         '''populate self.incomestatement_annual'''
-        html = get_source_code(
-            get_annual_incomestatement_url(
-                self.ticker)).split('Get Income Statement for:')[1]
+        try:
+            html = get_source_code(
+                get_annual_incomestatement_url(
+                    self.ticker)).split('Get Income Statement for:')[1]
 
-        self.incomestatement_annual = self.__get_endofperiod(html)
-        for field in self.incomestatement_fields:
-            self.incomestatement_annual[field] = request(html, field)
+            self.incomestatement_annual = self.__get_endofperiod(html)
+            for field in self.incomestatement_fields:
+                self.incomestatement_annual[field] = request(html, field)
 
-        print 'Annual income statement for ' + str(self.ticker) + ' successfuly obtained'
+            print 'Annual income statement for ' + str(self.ticker) + ' successfuly obtained'
+        except Exception,e:
+            print 'failed construct_incomestatement_annual for ' + self.ticker + ', ' + str(e)
 
     def __construct_incomestatement_quarterly(self):
         '''populate self.incomestatement_quarterly'''
+        try:
+            html = get_source_code(
+                get_quarterly_incomestatement_url(
+                    self.ticker)).split('Get Income Statement for:')[1]
 
-        html = get_source_code(
-            get_quarterly_incomestatement_url(
-                self.ticker)).split('Get Income Statement for:')[1]
+            self.incomestatement_quarterly = self.__get_endofperiod(html)
+            for field in self.incomestatement_fields:
+                self.incomestatement_quarterly[field] = request(html, field)
 
-        self.incomestatement_quarterly = self.__get_endofperiod(html)
-        for field in self.incomestatement_fields:
-            self.incomestatement_quarterly[field] = request(html, field)
-
-        print 'Quarterly income statement for ' + str(self.ticker) + ' successfuly obtained'
+            print 'Quarterly income statement for ' + str(self.ticker) + ' successfuly obtained'
+        except Exception, e:
+            print 'failed construct_incomestatement_quarterly for ' + self.ticker + ', ' + str(e)
 
     def __construct_balancesheet_annual(self):
         '''populate self.balancesheet_annual'''
-        html = get_source_code(
-            get_annual_balancesheet_url(
-                self.ticker)).split('Get Balance Sheet for:')[1]
+        try:
+            html = get_source_code(
+                get_annual_balancesheet_url(
+                    self.ticker)).split('Get Balance Sheet for:')[1]
 
-        self.balancesheet_annual = self.__get_endofperiod(html)
-        for field in self.balancesheet_fields:
-            self.balancesheet_annual[field] = request(html, field)
+            self.balancesheet_annual = self.__get_endofperiod(html)
+            for field in self.balancesheet_fields:
+                self.balancesheet_annual[field] = request(html, field)
 
-        print 'Annual balance sheet for ' + str(self.ticker) + ' successfuly obtained'
+            print 'Annual balance sheet for ' + str(self.ticker) + ' successfuly obtained'
+
+        except Exception, e:
+            print 'failed construct_balancesheet_annual for ' + self.ticker + ', ' + str(e)
 
     def __construct_balancesheet_quarterly(self):
         '''populate self.balancesheet_quarterly'''
-        html = get_source_code(
+        try:
+            html = get_source_code(
             get_quarterly_balancesheet_url(
                 self.ticker)).split('Get Balance Sheet for:')[1]
 
-        self.balancesheet_quarterly = self.__get_endofperiod(html)
-        for field in self.balancesheet_fields:
-            self.balancesheet_quarterly[field] = request(html, field)
+            self.balancesheet_quarterly = self.__get_endofperiod(html)
+            for field in self.balancesheet_fields:
+                self.balancesheet_quarterly[field] = request(html, field)
 
-        print 'Quarterly balance sheet for ' + str(self.ticker) + ' successfuly obtained'
+            print 'Quarterly balance sheet for ' + str(self.ticker) + ' successfuly obtained'
+
+        except Exception, e:
+            print 'failed construct_balancesheet_quarterly for ' + self.ticker + ', ' + str(e)
 
     def __construct_cashflow_annual(self):
         '''populate self.cashflow_annual'''
-        html = get_source_code(
-            get_annual_cashflow_url(
-                self.ticker)).split('Get Cash Flow for:')[1]
+        try:
+            html = get_source_code(
+                get_annual_cashflow_url(
+                    self.ticker)).split('Get Cash Flow for:')[1]
 
-        self.cashflow_annual = self.__get_endofperiod(html)
-        for field in self.cashflow_fields:
-            self.cashflow_annual[field] = request(html, field)
+            self.cashflow_annual = self.__get_endofperiod(html)
+            for field in self.cashflow_fields:
+                self.cashflow_annual[field] = request(html, field)
 
-        print 'Annual Cash Flows for ' + str(self.ticker) + ' successfuly obtained'
+            print 'Annual Cash Flows for ' + str(self.ticker) + ' successfuly obtained'
+
+        except Exception, e:
+            print 'failed construct_cashflow_annual for ' + self.ticker + ', ' + str(e)
 
     def __construct_cashflow_quarterly(self):
         '''populate self.cashflow_quarterly'''
-        html = get_source_code(
-            get_quarterly_cashflow_url(
-                self.ticker)).split('Get Cash Flow for:')[1]
+        try:
+            html = get_source_code(
+                get_quarterly_cashflow_url(
+                    self.ticker)).split('Get Cash Flow for:')[1]
 
-        self.cashflow_quarterly = self.__get_endofperiod(html)
-        for field in self.cashflow_fields:
-            self.cashflow_quarterly[field] = request(html, field)
+            self.cashflow_quarterly = self.__get_endofperiod(html)
+            for field in self.cashflow_fields:
+                self.cashflow_quarterly[field] = request(html, field)
 
-        print 'Quarterly Cash Flows for ' + str(self.ticker) + ' successfuly obtained'
+            print 'Quarterly Cash Flows for ' + str(self.ticker) + ' successfuly obtained'
+
+        except Exception, e:
+            print 'failed construct_cashflow_quarterly for ' + self.ticker + ', ' + str(e)
 
     def __construct_stockinfo(self):
         '''get basic information on the stock (sector, industry, nb of employees'''
-        html = get_source_code(get_stockinfo_url(self.ticker))
-        self.infos = get_infos(html)
+        try:
+            html = get_source_code(get_stockinfo_url(self.ticker))
+            self.infos = get_infos(html)
+
+        except Exception, e:
+            print 'failed construct_stockinfo for ' + self.ticker + ', ' + str(e)
 
     def __get_endofperiod(self, html):
         '''scrape the html source code for the ending periods of each column'''
@@ -151,4 +176,7 @@ class yahoo_ff:
 
     def __get_pricehistory(self):
         '''get stock price history and volume traded using quandl api'''
-        self.pricehistory = Quandl.get('WIKI/' + self.ticker)
+        try:
+            self.pricehistory = Quandl.get('WIKI/' + self.ticker)
+        except Exception, e:
+            print 'failed get_pricehistory for ' + self.ticker + ', ' + str(e)
