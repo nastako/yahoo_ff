971fb243e2f7d65032387fe0f008e34a2bb2c647
Sobo <sobolevski.a@gmail.com>
2016-02-06 23:54:28 -0500

    renamed to be more explicit

--- stocks_database.py
+++ stocks_database.py
@@ -2,32 +2,31 @@ from yahoo_ff import yahoo_ff
 import os
 import pickle
 
+
 # TODO check if exists already, check if all files are there
 
-class database:
+class stocks_database:
     '''creates a pickle database for a list of tickers specified in list.format file'''
-    format = '.txt'
+    format = '.csv'
 
     def __init__(self, list):
+
         self.name = list
-        self.filename = list + database.format # which file do you read to construct it
+        self.filename = list + stocks_database.format # which file do you read to construct it
         self.location = os.getcwd() + '/' + list + '_db/'
         if not os.path.exists(self.location):
             os.makedirs(self.location)
             self.__create()
+        else:
+            print 'database ' + self.name + ' already exists'
 
     def __create(self):
         # create the database with for loop, make pickle file for each
-        with open(self.filename, 'rb') as list:
-            for ticker in list:
-                try:
-                    ticker = ticker.strip()
-                    pickle.dump(yahoo_ff(ticker), open(self.location + ticker + '.p', 'wb'))
-                    print 'added ' + ticker + ' to database ' + self.name
-                except:
-                    print 'failed getting data for ' + ticker
-
-        return 0
+        with open(self.filename, 'r') as f:
+            tickers = (f.read().split())
+            for ticker in tickers:
+                pickle.dump(yahoo_ff(ticker), open(self.location + ticker + '.p', 'wb'))
+                print 'added ' + ticker + ' to database ' + self.name
 
     def take(self, ticker):
         # return the yahoo_ff object that was stored in a pickle file of the database
