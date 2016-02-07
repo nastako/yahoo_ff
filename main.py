aa11b0f0dcd141d0decc689e264114c19d3f07ac
Sobo <sobolevski.a@gmail.com>
2016-02-07 17:11:41 -0500

    housekeeping

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import pprint
 from stocks_database import stocks_database
-
+import pandas as pd
+from yahoo_ff import *
 
 
 def main():
     pp = pprint.PrettyPrinter(indent=0)
 
-    sp500test = stocks_database('sp500')
+    sp500test = stocks_database('sp500test')
 
+    #pp.pprint( sp500test.take('aapl').package_sec_annually().head())
+
+    #pp.pprint(sp500test.take('aapl').pricehistory.head())
 
 if __name__ == "__main__":
     main()
